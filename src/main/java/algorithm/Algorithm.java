ab23062ad7a8a6c9ebe5f3da5dc1d24d2ae5a625
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-20 01:30:19 +0200

    feat: add class for processing data

---
+++ src/main/java/algorithm/Algorithm.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import io.input.InputData;
+import io.output.OutputForm;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Algorithm {
+    public OutputForm perform(InputData inputData) {
+        return null;
+    }
+}
