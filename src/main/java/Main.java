18eadd460bb9d7d07ee0aad9abb1d11df6904e89
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-20 01:40:02 +0200

    feat: invoke all classes from Main.class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main (String[] args) {
+import io.input.InputManager;
 
+import static algorithm.Algorithm.perform;
+import static io.output.Printer.print;
+
+public class Main {
+    private static String[] names = new String[] {"b_small", "c_medium", "d_quite_big", "e_also_big"};
+    public static void main (String[] args) {
+        for (String name: names) {
+            print(perform(new InputManager(name + ".in").scan()), name + ".out");
+        }
     }
 }
