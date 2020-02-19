aba48e284fc4cd815b364ed541e474ea425185e6
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-19 20:03:23 +0200

    feat: override method OutputForm#toString()

--- src/main/java/io/output/OutputForm.java
+++ src/main/java/io/output/OutputForm.java
@@ -1,8 +1,19 @@
 package io.output;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import static java.util.Arrays.stream;
+
 @Data
+@AllArgsConstructor
 public class OutputForm {
     private int[] typesOfPizza;
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stream(typesOfPizza).forEach(type -> stringBuilder.append(type).append(" "));
+        return stringBuilder.deleteCharAt(stringBuilder.length()-1).toString();
+    }
 }
