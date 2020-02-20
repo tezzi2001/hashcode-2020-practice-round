56878fe7086bb82202c99b24fd00493395835dcc
Bohdan <bohdan.bondarenko@gmail.com>
2020-02-20 02:58:23 +0200

    feat: add first version of algorithm

--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -7,6 +7,24 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Algorithm {
     public OutputForm perform(InputData inputData) {
-        return null;
+        int[] inputArray = inputData.getArray();
+        int sum = 0;
+        int maxSum = 0;
+        int i;
+        for (i = 0; i < inputArray.length;  i++) {
+            sum += inputArray[i];
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            if (sum > inputData.getMax()) {
+                System.out.println(sum-inputArray[i]);
+                int[] array = new int [i];
+                for (int j = 0; j < i; j++) {
+                    array[j] = inputArray[j];
+                }
+                return new OutputForm(array);
+            }
+        }
+        return new OutputForm(inputArray);
     }
 }
